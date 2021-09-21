c77681e4d7a080e8fe737bf80bdfe93ad979f8f2
Barbara Gaweł-Kucab <90979294+bgawkuc@users.noreply.github.com>
2021-09-21 04:01:18 +0200

    Update 1.4_min&max.py

--- ASD_tasks/1-4_sorting/ASD_1/1.4_min&max.py
+++ ASD_tasks/1-4_sorting/ASD_1/1.4_min&max.py
@@ -1,24 +1,22 @@
 #Proszę zaimplementować funkcję, która mając na wejściu tablicę n elementów
 #oblicza jednocześnie jej największy i najmniejszy element wykonując 1.5n porównań.
 
-#sprawdzam elementy parami, mniej porownan
-def minmax(T):
-    mini = T[-1] #daje na ost el
-    maxi = T[-1]
-    for i in range(0,len(T)-1,2):
+#sprawdzając elementy parami, mam mniej porównań
+def minmax(A):
+    mini = A[-1]
+    maxi = A[-1]
+    for i in range(0,len(A)-1,2):
 
-        if T[i] < T[i+1]: #porownuje elementy parami
-            if T[i] < mini: #ten mniejszy jest kandedatem na mini
-                mini = T[i]
-            if T[i+1] > maxi:
-                maxi = T[i+1]
+        if A[i] < A[i+1]: 
+            if A[i] < mini:
+                mini = A[i]
+            if A[i+1] > maxi:
+                maxi = A[i+1]
 
-        else: #T[i] >= T[i+1] #wiekszy jest kandydatem na maxi
-            if T[i] > maxi:
-                maxi = T[i]
+        else:
+            if A[i] > maxi:
+                maxi = A[i]
             if T[i+1] < mini:
-                mini = T[i+1]
+                mini = A[i+1]
 
     return mini,maxi
-
-print(minmax([7,2,8,4,1,9]))
