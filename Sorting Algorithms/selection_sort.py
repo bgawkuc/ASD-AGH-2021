d515567477b33d948f734e177692557f28713470
Barbara Gaweł-Kucab <90979294+bgawkuc@users.noreply.github.com>
2021-09-21 03:46:52 +0200

    Update selection_sort.py

--- Sorting Algorithms/selection_sort.py
+++ Sorting Algorithms/selection_sort.py
@@ -1,10 +1,6 @@
 #sortowanie przez wybieranie
 #O(n^2)
 
-#przechodze liniowo po tablicy wybieram i-ty element
-#patrze na wszystkie elementy na prawo od niego i szukam tam najmniejszego
-#zamieniam element i-ty z najmniejszym znalezionym po jego prawej
-
 def selectionSort(A):
     for i in range(len(A)):
         mini = i
@@ -15,6 +11,3 @@ def selectionSort(A):
         A[i], A[mini] = A[mini], A[i]
 
     return A
-
-A = [9,2,7,1,0,3]
-print(selectionSort(A))
