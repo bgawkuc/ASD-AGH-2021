f5fff105468a416f450f77de35a1cb297b0bca80
Barbara Gaweł-Kucab <90979294+bgawkuc@users.noreply.github.com>
2021-09-21 04:09:27 +0200

    Update 1.7_sum_of_2_el.py

--- ASD_tasks/1-4_sorting/ASD_1/1.7_sum_of_2_el.py
+++ ASD_tasks/1-4_sorting/ASD_1/1.7_sum_of_2_el.py
@@ -1,24 +1,16 @@
-#dana jest posortowana tablica oraz liczba x.
-#proszę napisać program, który stwierdza czy istnieją indeksy i oraz j
-#takie, że A[i] + A[j] = x.
-
-#ustawiam i na poczatkowy idx a j na koncowy idx
-#jesli idx i >= j to zwracam False
-#jesli suma A[i] + A[j] jest mniejsza od x to zwiekszam idx i
-#jesli suma A[i] + A[j] jest wieksza od x zmniejszam idx j
-#jesli suma A[i] + A[j] jest rowna x to zwiacam A[i] oraz A[j]
-
+#Dana jest posortowana tablica A[1...n] oraz liczba x. Proszę napisać program,
+#który stwierdza czy istnieją indeksy i oraz j takie, że A[i] + A[j] = x.
 
 def exist(A,i,j,x):
     if i >= j:
         return False
-    if A[i] + A[j] > x: #gdy suma jest za duza to z prawej zmniejszam zakres
+    #gdy suma jest za duża zmniejsza prawy indeks
+    if A[i] + A[j] > x:
         return exist(A,i,j-1,x)
-    elif A[i] + A[j] < x: #gdy suma jest za mala to z lewej zwiekszam zakres
+    #gdy suma jest za mała zwiększa lewy indeks
+    elif A[i] + A[j] < x: 
         return exist(A,i+1,j,x)
     else :
-        return A[i], A[j]
+        return True
 
-t = [1,3,4,8,11]
 
-print(exist(t,0,len(t)-1,12))
