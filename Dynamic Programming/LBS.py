66c1ee492484947a1ec6a81b2e86d25f9e29fd48
Barbara Gaweł-Kucab <90979294+bgawkuc@users.noreply.github.com>
2021-09-22 14:22:41 +0200

    Update LBS.py

--- Dynamic Programming/LBS.py
+++ Dynamic Programming/LBS.py
@@ -1,16 +1,13 @@
-#longest bitonic subsequence
-#najdluzszy podciag bitoniczny
-#ktory najpierw rosnie a potem maleje
-#nie musi byc spojny
+#Znajdż LBS(longest bitonic subsequence), czyli najdłuższy podciąg, który najpierw rośnie, a potem maleje.
 
-#obliczam dlg najdluzszego podciagu rosnącego konczacego sie w i
-#obliczam najdluzszego podciagu malejacego zaczynajacego sie w i
-#potem szukam dla jakiego i suma tych wartosci jest maksymalna
-#na podstawie tego indeksu odtwrazam ciąg malejący oraz rosnacy
+
+#Obliczam: dlg najdluzszego podciagu rosnącego konczacego sie w i, dlg najdluzszego podciagu malejacego zaczynajacego sie w i.
+#Na podstawie tych danych znajduje długość LBS i go odtwarzam.
+#Złożoność: O(n^2), n - rozmiar listy wejściowej
 
 def LBS(A):
     n = len(A)
-    inc = [1] * n #dlg podciagu rosnaego konczacego sie w i
+    inc = [1] * n #dlg podciagu rosnacego konczacego sie w i
     pi = [-1] * n #ostatni idx w rosnącym
     dec = [1] * n #dlg podciagu malejacego zaczynajacego sie w i
     pd = [-1] * n #pierwszy idx w malejącym
@@ -18,18 +15,18 @@ def LBS(A):
     #szukam najdluzszego podciagu rosnącego konczacego sie w i
     for i in range(1,n):
         for j in range(i):
-            if A[i] > A[j] and inc[j] + 1 > inc[i]: #przeszukuje na lewo elementy
+            if A[i] > A[j] and inc[j] + 1 > inc[i]:
                 inc[i] = inc[j] + 1
                 pi[i] = j
 
     #szukam najdluzszego podciagu malejacego zaczynajacego sie w i
     for i in range(n-2,-1,-1):
         for j in range(i+1,n):
-            if A[i] > A[j] and dec[j] + 1 > dec[i]: #przeszukuje na prawo elementy
+            if A[i] > A[j] and dec[j] + 1 > dec[i]:
                 dec[i] = dec[j] + 1
                 pd[i] = j
 
-    #szukam dla jakiego idx suma podciagu rosnącego i malejącego jest maksymalna
+    #szukam dla jakiego indeksu suma podciagu rosnącego i malejącego jest największa
     maxi = 0
     for i in range(1,n):
         if inc[i] + dec[i] > inc[maxi] + dec[maxi]:
@@ -44,17 +41,10 @@ def LBS(A):
     #wypisuje podciąg malejący
     def solutionDec(i):
         if pd[i] != -1:
-            if i != maxi: #zeby dwa razy nie wypisac tej samej wartosci
+            if i != maxi:
                 print(A[i],end=" ")
             solutionDec(pd[i])
 
     solutionInc(maxi)
     solutionDec(maxi)
-    print()
-
-    return inc[maxi] + dec[maxi] #dlg takiego podciagu
-
-A = [4,2,5,7,6,9,1]
-print(LBS(A))
-
-
+   
