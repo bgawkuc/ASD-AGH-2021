efaf3244c5db73e86e19489ace03965c4fb4e2c2
Barbara Gaweł-Kucab <90979294+bgawkuc@users.noreply.github.com>
2021-09-22 14:50:18 +0200

    Update fibonacci.py

--- Dynamic Programming/fibonacci.py
+++ Dynamic Programming/fibonacci.py
@@ -1,22 +1,12 @@
-#problem ciagu fibonacciego
-#ciag fib(0 po to by miec cos na 0 indeksie): 0,1,1,2,3,5,8,13...
-#kazda liczba powstaje jako suma dwoch poprzednich
-#tworze tablice w ktorej pod kazdym ind znajduje sie kolejny wyraz ciagu fib
-#na koncu sciagam tylko ostatni el
-#zlozonosc O(n)
+#Wypisz n-ty wyraz ciągu Fibonacciego.
+#Ciąg Fibonacciego to ciąg liczb, gdzie każda jest sumą dwóch poprzednich.
+#Pierwsze dwa elementy to 1 i 1.
 
-def fib(n): #n - ktory wyraz ciagu fib chce dostac(0 sie nie liczy)
-    if n <= 1: #dla wartosci mniejszych od 2
-        return n
+def fib(n):
+    A = [-1] * n
+    A[0], A[1], = 1, 1
 
-    T = [-1] * (n+1)
-    T[0], T[1] = 0, 1 #wypelniam 0 i 1 wyraz ciagu
-
-    for i in range(2,n+1): #kazdy kolejny jest sumą dwoch poprzednich
-        T[i] = T[i-2] + T[i-1]
-    return T[n] #pod ind n-tym znajduje sie n-ty wyraz
-
-print(fib(7))
-
-
-
+    for i in range(2,n): 
+        A[i] = A[i-2] + A[i-1]
+    
+    return A[n-1] 
