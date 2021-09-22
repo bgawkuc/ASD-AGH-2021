3eb72f1122c39669461eb182539cf9c1840d843c
Barbara Gaweł-Kucab <90979294+bgawkuc@users.noreply.github.com>
2021-09-22 13:17:04 +0200

    Update 3.1_quicksort.py

--- ASD_tasks/1-4_sorting/ASD_3/3.1_quicksort.py
+++ ASD_tasks/1-4_sorting/ASD_3/3.1_quicksort.py
@@ -1,29 +1,26 @@
-#quicksort ktory zuzywa jedynie O(logn) pamieci
-#zuzywa tyle pamieci tylko bo najpierw sie zajmujemy max przedzialem dlg n//2
-#i potem na kazdym kroku ten przedzail zmniejsza,y dopoki sie nie skonczy
-#a potem dopiero zaczynamy robic przedzial drugiej strony od pivota
+# Proszę zaimplementować algorytm QuickSort do sortowania n elementowej tablicy tak, żeby
+# zawsze używał najwyżej O(log n) dodatkowej pamięci na stosie, niezależnie od jakości podziałów w funkcji
+# partition.
 
-def partition(T,l,r):
-    pivot = T[r]
-    i = l - 1
+#rekurenycjnie wywoluje quickosrt zawsze dla mniejszego przedziału - mając do wyboru od l do q-1 i q+1 do r
+
+def partition(A,l,r):
+    pivot = A[r]
+    i = l
     for j in range(l,r):
-        if T[j] <= pivot:
+        if A[j] <= pivot:
+            A[i],A[j] = A[j],A[i]
             i += 1
-            T[i],T[j] = T[j],T[i]
-    i += 1
-    T[i],T[r] = T[r],T[i]
+    A[i],A[r] = A[r],A[i]
     return i
 
-def quicksort(T,l,r):
+def quicksort(A,l,r):
     while l < r:
-        q = partition(T,l,r)
+        q = partition(A,l,r)
+        #wywołanie rekurenycjne dla mniejszej części
         if q - l < r - q:
-            quicksort(T,l,q-1)
-            l = q + 1 #lewy indeks idzie za pivota by zaczac teraz wywolywac prawa czesc
+            quicksort(A,l,q-1)
+            l = q + 1 
         else:
-            quicksort(T,q+1,r)
-            r = q - 1 #prawy indeks idzie przed pivota by zaczacwywolywav lewa czesc
-
-t = [9,2,1,8,0,2,4,3]
-quicksort(t,0,len(t)-1)
-print(t)
+            quicksort(A,q+1,r)
+            r = q - 1
