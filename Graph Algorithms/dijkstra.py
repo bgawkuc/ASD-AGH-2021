d3a677da0f5f8491ceb8078dc2c9b5df84239b24
Barbara Gaweł-Kucab <90979294+bgawkuc@users.noreply.github.com>
2021-09-23 21:46:55 +0200

    Update dijkstra.py

--- Graph Algorithms/dijkstra.py
+++ Graph Algorithms/dijkstra.py
@@ -1,120 +1,61 @@
-#algorytm do szukania najkrótszych ścieżek w grafie
-#działa jak BFS z sztucznymi wierzchołkami
-#z tą różnicą ze poza rozwazaniem sztucznych wierzchołków
-#patrzy tez na te oryginalne
-#NIE ZADZIAŁA DLA KRAWĘDZI O UJEMNYCH WAGACH
+#Algorytm Dijkstry - szuka najkrótszych ścieżek w grafie o nieujmenych wagach krawędzi.
+#Dwa rodzaje implementacji:
+#1) z kolejką priorytetową dla reprezentacji przez listy sąsiedztwa - O(ElogV)
+#2) bez kolejki priorytetowej dla reprezentacji macierzowej - O(V^2)
 
-#działa tylko dla wag dodadatnich(0 tez jest dodatnie)
-
-#algorytm
-#s-startowy
-#umiesc wszystkie wierzcholki w kolejce priorytetowej
-#oz szacowaniem odlgl od S rownym s
-#zmien odgls s na 0
-#poki są wierzchołki w kolejce:
-#wyjmij z kolejki wierzcholek u o minimalnym
-#oszacowaniu odległości
-#dla kazdej krawędzi {u,v} wykonaj relaksacje
-
-#relaksacja:
-#def relax(u,v):
-#   if v.d > u.d + w({u,v}): $=#czyli mozna poprawic oszacowanie dojscia do u
-#       v.d = u.d + w({u,v}) to poprawiam oszacowanie
-#       v.parent = u
-
-#O(ElogV) - dla linked_list wierzchołków
-
-
-#opis
-#reprezentacja dla listy sąsiadów
-#kazdy wierzołek dodaje do kolejki z odlegloscią inf
-#s - startowy
-#tworze tablice dist - min odlg z wierzcholka s do i -tego
-#i tablice parent - rodzic dla kazdego wierzcholka w najkrotszej trasie do niego
-#z kolejki wyciągam wierzcholek u o najmniejszej wartości odległości
-#sprawdzam jego sasiadow v
-#jesli odleglosc dla jakiegos wierzcholka v do s jest wieksza niz
-#niz odglegosc z s do u + krawedz {u,v}
-#to aktualizuje odgl do v na suma odlg z s do u + {u,v}
-#do kolejki wkładam wierzch v ze zmienioną, mniejszą odległością
-#parentem v staje się teraz u
-#ten proces sie powtarza dopoki kolejka nie zostanie pusta
-#zwracam parentow i odleglosci
-#wiec mam dostep do odlg od s do kazdego innego i moge odtworzyc trase od s do tego wierzch
-
+#1)Implementacja dla list sąsiedztwa
+#Złożoność: O(ElogV) 
 from queue import PriorityQueue
 
-#listy adj z pq
-#O(ElogV)
-def dijkstra(G,s):
+def dijkstra(G,s,t):
     n = len(G)
-    p = PriorityQueue()
     inf = float("inf")
-
-    p.put((0,s))
 
     parents = [None] * n
     visited = [False] * n
-    dist = [inf] * n #tablica najmniejszych odleglosci od wierzcholka s
-    dist[s] = 0 #odlgl od s do s wynosi 0
+    p = PriorityQueue()
+    
+    #wkładam do kolejki krotki: (odległość,wierzchołek)
+    p.put((0,s))
+    dist = [inf] * n 
+    dist[s] = 0 
 
     while not p.empty():
-        _, u = p.get() #wyjmuje krotke (_ = odleglosc, u = wierzcholek) po najmniejszych odleglosciach
+        _, u = p.get() 
         visited[u] = True
 
-        for i in range(len(G[u])): #szukam wsrod sasiadow wierzcholka u
-            v, edge = G[u][i] #sąsiad u reprezantowany jako krotka (v-wierzch,w-waga),
+        for v,edge in G[u]: 
 
-            #relaksacja, czyli jak istnieje krótsza ścieżka
-            # #jesli dlg trasy do wierzch v jest wieksza od trasy do wierzch u + wartosc krawedzi od u do v(w)
+            #relaksacja
             if dist[v] > dist[u] + edge and not visited[v]:
-                dist[v] = dist[u] + edge #aktualizuje dlg trasy do v
-                p.put((dist[v],v)) #wsadzam wierzcholek v z zaktualizowaną dlg drogi
-                parents[v] = u #aktualizuje rodzica v na u
-
-
-    return dist, parents
-
-#odtwarzanie ściezki z s do e
-def findPath(G,s,e):
-    dist, par = dijkstra(G,s)
+                dist[v] = dist[u] + edge 
+                parents[v] = u 
+                p.put((dist[v],v))
+                
+    #odtwarza wygląd najkrótszej ściezki z s do t
+    curr = t
     path = []
-    path.append(e)
-    el = par[e]
+    while curr is not None:
+        path.append(dist[curr])
+        curr = parents[curr]
+    
+    #zwraca minimalną ścieżkę z s do t i jej długość
+    return path[::-1],dist[t]
 
-    while el is not None:
-        path.append(el)
-        el = par[el]
-    return path[::-1]
+#2)Implementacja dla reprezentacji macierzowej
+#Złożoność: O(V^2)
 
-#(vertex,edge)
-G = [
-    [[1,3],[4,10]],
-    [[0,3],[2,6],[3,4]],
-    [[1,6],[3,1]],
-    [[1,4],[2,1],[4,2]],
-    [[0,10],[3,2]],
-]
-print(dijkstra(G,0))
-
-
-#dijikstra dla macierzowej repr bez PQ
-#LEPSZA ZŁOŻONOŚĆ!
-#O(V^2)
-
-#szukam v o min dist ktory nie był jeszcze odwiedzony
+#szuka wierzchołka, o minimalnej wartości dist, który nie był jeszcze odwiedzony
 def getMinVertex(visited, dist):
-    v = -1
-    d = float("inf")
+    v = None
 
     for i in range(len(dist)):
-        if not visited[i] and dist[i] < d:
-            d = dist[i]
+        if not visited[i] and (v is None or dist[v] > dist[i]):
             v = i
     return v
 
 
-def dijikstra4(G,s):
+def dijkstra(G,s,t):
     n = len(G)
     inf = float("inf")
 
@@ -124,26 +65,27 @@ def dijikstra4(G,s):
     dist[s] = 0
 
     for _ in range(n):
-        #wierzcholek o min dist ktory nie byl jeszcze odwiedzony
         u = getMinVertex(visited,dist)
+        if u is None:
+            break
         visited[u] = True
 
         for v in range(n):
             edge = G[u][v]
-
+            
+            #0 - oznacza brak krawędzi
             if edge != 0:
+                #relaksacja
                 if not visited[v] and dist[v] > dist[u] + edge:
                     dist[v] = dist[u] + edge
                     parent[v] = u
-
-    return dist, parent
-
-G = [
-    [0,1,0,2,0,10],
-    [1,0,2,0,5,0],
-    [0,2,0,3,0,4],
-    [2,0,3,0,8,0],
-    [0,5,0,8,0,1],
-    [10,0,4,0,1,0],
-]
-print(dijikstra4(G,0))
+    
+    #odtwarza wygląd najkrótszej ściezki z s do t
+    curr = t
+    path = []
+    while curr is not None:
+        path.append(dist[curr])
+        curr = parents[curr]
+        
+    #najkrótsza ściezka z s do t oraz jej długość
+    return path[::-1], dist[t]
