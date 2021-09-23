18aa7efc30dc08b2fd1ade0650928fe58a88cb9a
Barbara Gaweł-Kucab <90979294+bgawkuc@users.noreply.github.com>
2021-09-23 17:33:18 +0200

    Update 2.bricks.py

--- ASD_tasks/5-7_dp_greedy/ASD_6/2.bricks.py
+++ ASD_tasks/5-7_dp_greedy/ASD_6/2.bricks.py
@@ -1,43 +1,18 @@
-#mamy liste klockow kazdy o wysokosci jednostkowej
-#mamy podane wspolrzedna x-start oraz x-koniec kazdego klocka
-#mamy liste krotek z takimi wartosciami
-#napisz algorytm ktory oblicza ktore klocki nalezy usunac
-#aby liczba usuneietych klockow byla mozliwie jak najmniejsza
-#a kolejne klocki zawieraly sie w sobie nawzajem
-#czyli dla klocka x1, x2 to kolejne klocek z1,z2 musi:
-#miec wspolrzedne: z1 >= x1 i z2 <= x2,
-#tworzą nierozszerzającą się wieżę
-#kazdy kolejny ma sie zawierac w poprzednim
+# Każdy klocek to przedział postaci [a, b]. Dany jest ciąg klocków [a1, b1], [a2, b2], . . ., [an, bn]. 
+# Klocki spadają na oś liczbową w kolejności podanej w ciągu. Proszę zaproponować algorytm, który oblicza ile klocków 
+# należy usunąć z listy tak, zeby każdy kolejny spadajacy klocek mieścił  się w całości w tam, który spadł tuż przed nim.
 
-#dp[i] - max wysokosc wiezy jaką sie da uzysakc gdzie ostatnim klockiem jest i-ty klocek
-#wszystkie wartosci w dp ustawiam na 1
-#mam 2 pętlw
-#przechodze po i od 1 do n
-#przechodze po j od 0 do i-1
-#sprawdzam czy i-ty klocek zawiera sie w j-tym
-#oraz czy wybranie j-tego klocka pozowli mi zwiekszyc wysokosc wiezy
-#czyli czy dp[j] + 1 > dp[i]
-#wynik czyli minimalna ilosc klockow do usuniecia to
-#dlg tablicy z klockami - max wys wiezy
-
+#dp[i] - maksymalna wysokość wieży kończąca się na klocku o indeksie i
 
 def bricks(A):
     n = len(A)
-    #jaka max wieze da sie uzyskac biorac i-ty klocek
     dp = [1] * n #wys kazdej wiezy na starcie to 1
 
-    for i in range(1,n): #wybieram klocek i
+    for i in range(1,n): 
         for j in range(i):
-            #sprawdzam czy i-ty klocek zawiera sie w j-tym
-            #oraz czy biorac j-ty klocek wydluze wysokosc i-tej wiezy
+            #klocek o indeksie j musi mieć większe rozmiary niż ten o indeksie i
+            #oraz musi powiększać wysokość wieży
             if A[i][0] >= A[j][0] and A[i][1] <= A[j][1] and dp[i] < dp[j] + 1:
                 dp[i] = dp[j] + 1
 
-    #wynik to ilosc do usuniecia =  ilosc klockow - max wys wiezy
     return n - max(dp)
-
-#spadnie [1,5] -> [3,4] -> [3,3] tworzą wieze, reszte klockow trzeba usunac
-A = [[1,5],[2,7],[3,4],[1,10],[3,3]]
-print(bricks(A))
-
-
