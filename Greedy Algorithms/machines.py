7d5b24286d3eed72b1348621f5c904dbf4617ca5
Barbara Gaweł-Kucab <90979294+bgawkuc@users.noreply.github.com>
2021-09-23 18:40:09 +0200

    Update machines.py

--- Greedy Algorithms/machines.py
+++ Greedy Algorithms/machines.py
@@ -1,70 +1,31 @@
-#mam tablice 1D z miastami
-#kazde miasto ma wartosc 0 lub 1
-#w miastach oznaczonych 1 jest mozliwe zbudowanie maszyny chroniacej
-#mam tez wartosc k ktora mowi ze jesli w i postawimy maszyne
-#to miasta abs(i-j) < k sa chronione
-#napisz algorytm ktory mowi ile najmniej maszyn mozna zbudowac
-#by wszystkie miasta byly chronione
-
-#last - miejsca w ktorych wybudowano maszyne
-#notprotected-patrze do przodu szukajac mozliwie najdalszego miejsca(idx) by zbudowac maszyne
-#od notprotected w dol szukam 1 miejsca na ktorym jest 1
-#tam stawiam maszyne
-#wiec last dostaje wartosc notprotected
-#a not protected zwiekszam o 2k-1(najdalsze miejsce by ustawic nową maszyne)
-#bo ta daleka maszyna chroni k - 1 miast w tył a ta inna k - 1 miast w przód
-#i tak dopoki miejsce ustawienia ostatniej maszyny jest < n - k
-
+#Dostajesz na wejściu tablice A[n] i wartość k. Wartość pola A[i] wynosi 0 lub 1.
+#1 oznacza, że istnieje możliwość wybudowania maszyny w danym miejscu.
+#Jeśli maszynę wybudowano w i-tym miejscu to chroni ona: abs(i-j) < k.
+#Znajdź miejsca gdzie należy wybudować maszyny, by ich ilość była minimalna i każde miejsce w A było chronione.
+#Jeśli nie istnieje taki wybór punktów zwróć False.
 
 def machines(A,k):
     n = len(A)
-    lastMach = -1 #idx gdzie ostatnio wybudowalam maszyne
-    cnt = 0
-    machine = k - 1 # najdalszy idx na ktorym moge probowac ustawic maszyne
 
-    while lastMach < n - k: #dopoki ostatnie k miast nie jest chronionych
-
-        if machine >= n: #gdy wyjde poza zakres z miejscem dla maszyny
-            machine = n - 1
-
-        while A[machine] != 1 and machine != lastMach:#probujemy mozliwie jak najdalej umiescic maszyne
-            #drugi warunek-po to by nie pokryła sie ona z moja obecną maszyną
-            #gdy nie da sie na miejscu ustawic maszyny(bo ma wart 0) to przesuwam sie w lewo
-            machine -= 1
-
-        #gdy przeszłam wszystkie 2k - 1/ k - 1(przy 1 maszynie) pól z przodu i nie znazłam miejsca tzn ze nie jest to możliwe
-        if machine == lastMach: #moment gdy nie ma pola 1 w wybranym obszarze tzn nie bedzie dalo sie spelnic warunkow
-            return -1
-
-        lastMach = machine #miejsce w ktorym umieszczam maszyne
-        machine += 2 * k - 1 #najdalszy idx miasta w ktorym mozna probowac wybudowac maszyne
-        cnt += 1
-
-    return cnt
-
-#CZYTELNIEJSZA IMPLEMENTACJA
-def machines2(A,k):
-    n = len(A)
-
-    #szukam miejsca dla 1 maszyny
+    #szukam miejsca dla 1 maszyny (o jak najdalszym indeksie)
     first = None
     for i in range(k-1,-1,-1):
         if A[i] == 1:
             first = i
             break
-
+    #gdy nie wszystkie miejsca byłyby chronione
     if first is None:
         return False
     mach = [first]
 
-    #do jakiego indeksu mam chronione
+    #do jakiego indeksu do przodu mam chronione miejsca
     protected = first + k - 1
 
     #dopoki nie wszystkie pola są chronione
     while protected < n-1:
 
         new = None
-        #szukam jak najdalszego miejsca na maszyne
+        #szukam jak najdalszego miejsca na nową maszyne
         for i in range(protected+k-1,protected,-1):
             if i < n and A[i] == 1:
                 new = i
@@ -78,9 +39,3 @@ def machines2(A,k):
             protected = new + k - 1
 
     return mach
-
-arr=[1,0,0,0,1,0,0,1,0,0,1,0,1,0,1,0]
-a = [1,1,0,1,0,1,0]
-print(machines(arr,3))
-
-
