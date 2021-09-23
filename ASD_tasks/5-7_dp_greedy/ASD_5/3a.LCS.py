263f10ae503a6a0a3897c9a85af167e9e257bad0
Barbara Gaweł-Kucab <90979294+bgawkuc@users.noreply.github.com>
2021-09-23 16:59:15 +0200

    Update 3a.LCS.py

--- ASD_tasks/5-7_dp_greedy/ASD_5/3a.LCS.py
+++ ASD_tasks/5-7_dp_greedy/ASD_5/3a.LCS.py
@@ -1,59 +1,12 @@
-#najdluzszy wspolny podciag
-#longest common subsequence
-#mam tablice A i B dlg n kazda
-#sprawdz jakiej dlugosci jest najdluzszy wspolny podciag
+# Mamy dane dwie tablice, A[m] i B[n]. Należy znaleźć ich najdłuższy wspólny podciąg.
 
-#zlozonosc O(n*m)
-#f(i,j) =
-#1) 0 dla i = 0 lub j = 0
-#2) f(i-1,j-1) + 1 dla i,j >0 i Ai = Bj
-#3)max(f(i-1,j),f(i,j-1)) dla i,j > 0 i Ai != Bj
+#tworzę tablice dp 2d, rozmiaru długość słowa A na długość słowa B
+#dp[a][b]- dlg max wspolnego podciagu jaki sie da uzyskac patrzac na litery o indeksach od 0 do a w A i od 0 do b w B
 
-#tworze wymiarach dlg slowa A + 1 na dlg slowa B + 1
-#dp[a][b] dlg max wspolnego podciagu jaki sie da uzyskac patrzac na a liter z A i b liter z B
-
-def LCS(A,B):
-    m = len(A)
-    n = len(B)
-
-    #tablica rozmiarow dlg kazdego slowa + 1
-    dp = [[0] * (n+1) for _ in range(m+1)]
-
-    for a in range(1,m+1): #wyninek dlugosci a z ciągu A
-        for b in range(1,n+1): #wycnek dlugosci b z ciagu B
-
-            #gdy liczby o idx a-1 i b-1 sa rowne
-            #to pole dostaje wartosc taką jaka była na ukos + 1
-            #czyli max dlg podciagu jaka byla dla ciagow o dlg a-1 oraz b-1
-            if A[a-1] == B[b-1]:
-                dp[a][b] = dp[a-1][b-1] + 1
-
-            #gdy liczby są różne
-            #to biorę wieksza wartosc z ciagow dlg a-1 i b oraz ciagow dlg a i b-1
-            else:
-                dp[a][b] = max(dp[a-1][b], dp[a][b-1])
-
-    #odtwaram rozwiazanie
-    #od tyłu
-    lcs = []
-    i, j = m,n
-
-    while i > 0 and j > 0:
-        if A[i-1] == B[j-1]:
-            lcs.append(A[i-1])
-            i-= 1
-            j -= 1
-        elif dp[i-1][j] > dp[i][j-1]:
-            i -= 1
-        else:
-            j -= 1
-
-    return lcs[::-1], dp[m][n]
-
-#CZYTELNIEJSZY ZAPIS
-def lcs2(A, B):
+def lcs(A, B):
     a = len(A)
     b = len(B)
+    
     # slowo A- wiersze, słowo B - kolumny
     dp = [[0] * (b) for _ in range(a)]
 
@@ -70,11 +23,10 @@ def lcs2(A, B):
     for a1 in range(1, a):
         for b1 in range(1, b):
 
-            # jesli el są sobie równe to wynik to pole o kol i wier o 1 mniejszym + 1
+            # jesli litery pod indeksem a1 i b1 są takie same
             if A[a1] == B[b1]:
                 dp[a1][b1] = dp[a1 - 1][b1 - 1] + 1
-            # jesli sa rozne to wynik to max z slowa konczacych sie na kol o 1 mniejszej
-            # lub wierszu o 1 mniejszym
+            # jesli sa rozne
             else:
                 dp[a1][b1] = max(dp[a1 - 1][b1], dp[a1][b1 - 1])
 
@@ -90,10 +42,5 @@ def lcs2(A, B):
             a1 -= 1
         else:
             b1 -= 1
-    return res[::-1], dp[a - 1][b - 1]
-
-
-a = [1,9,8,2,5,6]
-b = [2,1,8,3,2]
-
-print(LCS(a,b))
+    
+    return res[::-1]
