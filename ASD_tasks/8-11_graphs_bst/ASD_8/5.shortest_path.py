03931303e63f20dd47ae6892a7afde4dfea6b7b8
Barbara Gaweł-Kucab <90979294+bgawkuc@users.noreply.github.com>
2021-09-23 23:54:15 +0200

    Update 5.shortest_path.py

--- ASD_tasks/8-11_graphs_bst/ASD_8/5.shortest_path.py
+++ ASD_tasks/8-11_graphs_bst/ASD_8/5.shortest_path.py
@@ -1,23 +1,17 @@
-#zmieniony algorytm BFS do znalezienia najkrotszej sciezki z s do e
-#i wypisania jej
-#implementacja przez listę linked_list
-#zlozonoszc O(v+e)
+# Proszę zaimplementować algorytm BFS tak, żeby znajdował najkrótsze ścieżki w grafie i następnie,
+# żeby dało się wypisać najkrotszą ścieżkę z zadanego punktu startowego do wskazanego wierzchołka.
 
-#wywoluje BFS dla wierzcholka startowego
-#trwa on dopoki queue nie bedzie puste(brak rozw)
-#albo pole koncowe zostanie odwiedzone
-#sasiadow wierzcholka startowego dodaje do kolejki
-#za kazdym razem zapamiętuje rodzica wierzcholka by móc odtworzyc sciezke
-#oraz za kkazdym razem licze odlgl danego wierzcholka od startowego
-#po wyjsciu z BFSa na podstawie tablicy rodzicow odtwarzam rozwiazanie
+#Z użyciem BFSa przechodzę po grafie zapisując odległość do danego wierzchołka od startowego w tablicy dist.
+#Na bazie tablicy rodziców odwzorowuje wygląd ścieżki.
+#Implementacja przez listy sąsiedztwa.
 
 from queue import Queue
 from math import inf
 
-def solution(G, s, e):
+def shortestPath(G, s, t):
     n = len(G)
-    parent = [-1] * n #rodzic wierzcholka o idx i
-    dist = [inf] * n #odleglosc wierzcholka i do s (startowego)
+    parent = [-1] * n 
+    dist = [inf] * n 
     visited = [False] * n
 
     def BFS(G):
@@ -27,40 +21,29 @@ def solution(G, s, e):
         q.put(s)
 
         while not q.empty():
-            u = q.get() #wyjmuje el z kolejki
+            u = q.get() 
 
-            for v in G[u]: #patrze po sąsiadach
+            for v in G[u]:
 
-                if not visited[v]: #gdy nie był jeszcze odwiedzony
+                if not visited[v]: 
                     visited[v] = True
                     dist[v] = dist[u] + 1
                     parent[v] = u
-                    q.put(v) #wkladam sąsiada do kolejki
-
-                    if v == e: #gdy okaze sie ze doszlam do koncowego to koncze BFSa
+                    q.put(v)i
+                    
+                    #gdy istnieje ścieżka z s do t
+                    if v == t: 
                         return True
         return False
-
-    if BFS(G) == False: #jesli nie istnieje droga do wierzcholka e
+    
+    #jesli nie istnieje ściezka z s do t
+    if not BFS(G): 
         return False
 
-    path = [] #rozwiazanie
-    path.append(e)
+    path = [] 
 
-    while parent[e] != -1: #odtwarzam sciezke od tyłu, tzn zaczynajac od wierzcholka e, patrzac na rodzicow wierzch
-        path.append(parent[e])
-        e = parent[e]
+    while t != -1: 
+        path.append(t)
+        t = parent[t]
 
-    return path[::-1], len(path)
-
-G =[
-     [1,3],
-     [2,0],
-     [1],
-     [4,7,0],
-     [3,7,6,5],
-     [6,4],
-     [7,4,5],
-     [6,4,3]
-     ]
-print(solution(G,2,6))
+    return path[::-1]
