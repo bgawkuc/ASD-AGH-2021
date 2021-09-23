3f0c71d7078aa7de762f1a778ecb0408101ad5eb
Barbara Gaweł-Kucab <90979294+bgawkuc@users.noreply.github.com>
2021-09-23 21:21:37 +0200

    Update DFS.py

--- Graph Algorithms/DFS.py
+++ Graph Algorithms/DFS.py
@@ -1,24 +1,14 @@
-#przeszukiwanie w głąb grafu
-#depth first search
-
-#  b - e
-# /    | \        h
-#a     d  \      /
-# \   /    f -- g
-#   c  -- /
-#idź poki możesz a jak sie nie da to sie cofaj
-#O(V+E) dla linked_list adj
+#DFS-Depth-first search, preszukiwanie w głąb
+#Implementacja dla list sąsiedztwa
+#Złożoność: O(V+E)
 
 def DFS(G,s):
     visited = [False] * len(G)
 
     def DFS_visit(u):
-        nonlocal G, visited
-
         visited[u] = True
         for v in G[u]:
             if not visited[v]:
                 DFS_visit(v)
 
     DFS_visit(s)
-
