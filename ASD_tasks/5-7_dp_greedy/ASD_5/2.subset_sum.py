b62db3ae97f50084472c27c3ac7a18c1444cfa39
Barbara Gaweł-Kucab <90979294+bgawkuc@users.noreply.github.com>
2021-09-23 16:47:04 +0200

    Update 2.subset_sum.py

--- ASD_tasks/5-7_dp_greedy/ASD_5/2.subset_sum.py
+++ ASD_tasks/5-7_dp_greedy/ASD_5/2.subset_sum.py
@@ -11,14 +11,15 @@ def subsetSum(A,sum_):
     #sumę 0 zawsze się da utworzyć
     for i in range(n+1): 
         dp[i][0] = True
-
+    
+    #i-którą z kolei liczbę wybieram, j-obecna wartosć sumy
     for i in range(n+1): 
         for j in range(1,sum_+1):
 
             if j < A[i-1]:
                 dp[i][j] = dp[i-1][j]
 
-            #gdy jest szansa by zabrac moją i-tą liczbe
+            #gdy jest szansa wybrać do sumy i-tą liczbe
             else:
                 dp[i][j] = (dp[i-1][j] or dp[i-1][j-A[i-1]])
                 
