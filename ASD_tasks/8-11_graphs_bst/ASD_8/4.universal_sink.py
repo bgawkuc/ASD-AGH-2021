a49ff95b35c623b98b9ad5dfb9a8a6621a0994eb
Barbara Gaweł-Kucab <90979294+bgawkuc@users.noreply.github.com>
2021-09-23 23:48:15 +0200

    Update 4.universal_sink.py

--- ASD_tasks/8-11_graphs_bst/ASD_8/4.universal_sink.py
+++ ASD_tasks/8-11_graphs_bst/ASD_8/4.universal_sink.py
@@ -1,31 +1,30 @@
-#mowimy ze wierzcholek t w grafie skierowanym jest uniwersalnym ujsciem jesli
-#a) z kazdego innego wierzcholka v istnieje krawedz z v do t
-#b) nie istnieje krawędź wychodząca z t
-#podaj algorytm znajdujacy ujscie postaci macierzowej grafu
+# Mówimy, że wierzchołek t w grafie skierowanym jest uniwersalnym ujściem, jeśli:
+# (a) z każdego innego wierzchołka v istnieje krawędź z v do t
+# (b) nie istnieje żadna krawędź wychodząca z t.
+# Podaj algorytm znajdujący uniwersalne ujście (jeśli istnieje) przy reprezentacji macierzowej.
 
-#warunek b->szukam takiego wiersza u ktory jest wypelniony tylko 0
-#warunek a-> szukam takiej kolumny u wypelnionej samymi 1(poza M[u][u])
-#gdy M[u][v] = 0 tzn ze krawedzi z u do v nie ma czyli v nie moze byc ujsciem
-#gdy M[u][v] = 1 tzn ze z u do v jest krawedz to u nir moze byc ujsciem
-#liniowo szukam kandydata na ujscie
-#gdy M[u][v] == 0 to v++ ruch w prawo #potencjalnie dobry wiersz
-#gdy M[u][v] == 1 to u++ ruch w dół #na pewno zly wiersz
-#kiedy dojdziemy z v poza ostatnią kolumne tzn ze wierzcholek u jest kandydatem
-#i tylko dla niego sprawdzamy czy u-ty wiersz sklada sie z samych zer
-# a cata u-ta kolumna z samych 1(z wyjatkiem u-tego el)
-#jesli wyjdziemy z v za ostatni wiersz tzn ze ujscia nie ma
+#Szukam takiego wierzchołka u, dla którego cały wiersz G[u] ma wypełniony wartością 0, a kolumna jest cała wypełniona 1 (poza G[u][u]).
+#Szukam potencjalnego wierzchołka w grafie, który mógłby być ujściem poruszając się w prawo/ w dół w zależności od wartości.
+#I dla niego dfsem sprawdzam czy jest ujściem.
+#Złożoność: O(V)
 
 def universal_sink(G):
     n = len(G)
+    #u-wiersz,v-kulumna
     u, v = 0, 0
-
-    while u < n and v < n: #poruszmam sie odpwiednio w prawo lub w dół
+    
+    #szukam wierzchołka, który mógłby być potencjalnym ujściem
+    while u < n and v < n:
 
         if G[u][v] == 0:
             v += 1
 
         else:
             u += 1
+            
+    #gdy wyjdę wierszem poza tablice
+    if u == n: 
+        return 'brak ujscia'
 
     def is_sink(u): #sprawdza czy dany wierzcholek jest ujsciem
         for v in range(n):
@@ -37,24 +36,9 @@ def universal_sink(G):
                 return False
 
         return True
-
-    if u == n: #gdy wyjdę wierszem poza tablice
+    
+    #sprawdzam czy wierzchołek o indeksie u jest ujściem    
+    if is_sink(u): 
+        return u
+    else:
         return 'brak ujscia'
-
-    else: #gdy wyjde kolumna poza tablice
-
-        if is_sink(u): #sprawdzam czy u-ty wierzcholek jest ujsciem
-            return u
-
-        else:
-            return 'brak ujscia'
-
-#graf posiadajacy ujscie dla indeksu 3
-G = [
-    [0,0,1,1,1],
-    [0,0,0,1,1],
-    [1,0,0,1,1],
-    [0,0,0,0,0],
-    [0,1,0,1,0],
-]
-print(universal_sink(G))
