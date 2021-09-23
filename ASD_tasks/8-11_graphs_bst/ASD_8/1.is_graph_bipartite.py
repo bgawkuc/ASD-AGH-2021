5e46a19c32e04be48cd2f090662aff050f1ba3eb
Barbara Gaweł-Kucab <90979294+bgawkuc@users.noreply.github.com>
2021-09-23 23:40:36 +0200

    Update and rename 3.is_graph_bipartite.py to 1.is_graph_bipartite.py

---
+++ ASD_tasks/8-11_graphs_bst/ASD_8/1.is_graph_bipartite.py
@@ -0,0 +1,34 @@
+#Sprawdz czy zdany graf jest dwudzielny (rownowazne kolorowaniu grafu 2 kolorami).
+
+#Wierzchołki w grafie koloruje na 2 kolory: 0 i 1.
+#Aby był dwudzielny to nie może pojawić się sytuacja, w której wierzchołki należace do 1 krawędzi miałyby ten sam kolor.
+#Implementacja przez listy sąsiedztwa.
+#Złożoność: O(V+E).
+
+from queue import Queue
+
+def BFS(G):
+    q = Queue()
+    visited = [False] * len(G)
+    color = [-1] * len(G)
+
+    q.put(0)
+    visited[0] = True
+    color[0] = 1 
+
+    while not q.empty():
+        u = q.get()
+
+        for v in G[u]:
+
+            if not visited[v]:
+                visited[v] = True
+                #koloruje na inny kolor niż ma u
+                color[v] = 1 - color[u] 
+                q.put(v)
+            
+            #gdy kolor wierzchołki na jednej krawędzi jest taki sam
+            if color[u] == color[v]:
+                return False
+
+    return True
