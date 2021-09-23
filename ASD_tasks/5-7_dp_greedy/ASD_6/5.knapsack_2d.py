8cf45057148bd3d15fbc446907fb3ae0e65fa6c0
Barbara Gaweł-Kucab <90979294+bgawkuc@users.noreply.github.com>
2021-09-23 17:45:33 +0200

    Update 5.knapsack_2d.py

--- ASD_tasks/5-7_dp_greedy/ASD_6/5.knapsack_2d.py
+++ ASD_tasks/5-7_dp_greedy/ASD_6/5.knapsack_2d.py
@@ -1,16 +1,18 @@
-#dwuwymiarowy problem
-#mam ograniczenie w plecaku na wage i wysokosc
-#wyznacz max profit
+
+# Proszę zaproponować algorytm dla dwuwymiarowej wersji dyskretnego problemu plecakowego.
+# Mamy dany zbiór n przedmiotów i dla każdego przedmiotu dane sa nastepujace trzy liczby: 
+#P[i] - cena, W[i] - waga, H[i] - wysokosć. Mamy ograniczenie plecaka w postaci maksymalnej wagi i wysokości.
+
+#dp[i][w][h] - max zysk jaki da sie uzyskac patrzac na przedmioty o indeksach od 0 do i oraz mając ograniczenie wagi w i wysokosci h
 
 def knapsack(P, W, H, maxWeight, maxHeight):
     n = len(P)
-    # dp[i][w][h] - max profit gdy patrze do i-tego przedmiotu włącznie
-    # do wagi w i wys h
+  
     dp = [[[0 for _ in range(maxHeight + 1)]
            for _ in range(maxWeight + 1)]
           for _ in range(n)]
 
-    # gdy zerowy przedmiot się zmiesci
+    # gdy przedmiot o indeksie 0 się zmiesci
     for w in range(W[0], maxWeight + 1):
         for h in range(H[0], maxHeight + 1):
             dp[0][w][h] = P[0]
@@ -20,7 +22,6 @@ def knapsack(P, W, H, maxWeight, maxHeight):
             for h in range(maxHeight + 1):
 
                 # gdy i-ty przedmiot sie zmiesci
-                #to sprawdzam czy bardziej sie oplaca go wybierać czy nie
                 if W[i] >= w and H[i] >= h:
                     dp[i][w][h] = max(dp[i][w][h], dp[i - 1][w - W[i]][h - H[i]] + P[i])
                 else:
