b861b620a1da6f15eacfddd0d8344ce0d9d1ebed
Barbara Gaweł-Kucab <90979294+bgawkuc@users.noreply.github.com>
2021-09-24 17:28:41 +0200

    Update 2.good_start.py

--- ASD_tasks/8-11_graphs_bst/ASD_9/2.good_start.py
+++ ASD_tasks/8-11_graphs_bst/ASD_9/2.good_start.py
@@ -1,16 +1,9 @@
-#mam graf skieorwany
-#sprawdz czy istnieje w nim dobry początek
-#to taki wierzcholek z ktorego da sie dotrzec do kazdego innego wierzcholka poprzez sciezke skierowana
-#czyli nie musi wychodzi krawedzz  niego bezposrednio do kazdego innego
-#wystarczy aby było posrednie dojscie do kazdego innego
+# Wierzchołek v w grafie skierowanym nazywamy dobrym początkiem jeśli każdy inny wierzchołek można 
+# osiągnąć scieżką skierowaną wychodzącą z v. Proszę podać algorytm, który stwierdza czy dany graf zawiera dobry początek.
 
-#wywoluje dfs w ktorym zapisuje czas dotarcia do kazdego wierzcholka
-#czasy dotarcia zaczynam od 1
-#szukam wierzcholka dla ktorego czas dotarcia jest rowny rozmiarowi grafu
-#bo jest wtedy szansa ze odiwedzilam wszystkie inne i na koncu trafilam do tegp
-#sprawdzam czy da sie z niego dotzrec do kazdego wierzcholka
-#sprawdzam to za pomocą DFS czy da mi on visited True dla kazdego wierzchołka
-#jesli tak to jest on dobrym początkiem
+#Wywołuje DFS dla grafu i zapisuje czas przetworzenia każdego wierzchołka.
+#szukam wierzchołka, dla którego czas przetworzenia wynosi tyle co rozmiar grafu - jest od potencjalnym dobrym początkiem.
+#Dla takiego wierzchołka wywołuje DFS i sprawdzam czy zostaną odwiedzone wszystkie wierzchołki.
 
 def goodStart(G):
     n = len(G)
@@ -26,20 +19,17 @@ def goodStart(G):
             if not visited[v]:
                 dfsVisit(v)
 
-        # czas pzretworzenia wierzcholka
         time += 1
         finish[u] = time
 
-    # wywoluje dfs dla kazdego nieodwiedzonego wierzcholka
     for u in range(n):
         if not visited[u]:
             dfsVisit(u)
 
     for u in range(n):
 
-        # jesli znajde wierzcholek ktorego czas przetworzenia wynosi tyle co rozmiar grafu
-        # to moze on byc potencjalnie dobrym wierzcholkiem
-        # wywołuje dla niego dfs i sprawdzam czy dotre do kazdego innego wierzcholka
+        # jesli znajde wierzcholek, ktorego czas przetworzenia wynosi tyle co rozmiar grafu
+        # to wywołuje dla niego dfs i sprawdzam czy zostanie odwiedzony każdy wierzchołek
         if finish[u] == n:
             visited = [False] * n
             dfsVisit(u)
@@ -47,17 +37,7 @@ def goodStart(G):
             for i in range(n):
                 if visited[i] == False:
                     return False
+            #zwracam dobry początek
             return True, u
 
     return False
-
-
-# 3 to dobry początek
-G = [
-    [],
-    [0],
-    [],
-    [1, 2],
-]
-print(goodStart(G))
-
