4ddef92ccb20a17880ecc9ed7b9c1fb5aeb5927e
Barbara Gaweł-Kucab <90979294+bgawkuc@users.noreply.github.com>
2021-09-24 17:22:45 +0200

    Update 1.hamilton_path.py

--- ASD_tasks/8-11_graphs_bst/ASD_9/1.hamilton_path.py
+++ ASD_tasks/8-11_graphs_bst/ASD_9/1.hamilton_path.py
@@ -1,16 +1,9 @@
-#sciezka hamiltona
 
-#to taka sciezka ktora przechodzi po wszystkich wierzcholkach a krawedzie sie nie powtarzaja
-#jest to prblem NP-trudny
-#znajdz sciezke hamiltona w acyklicznym grafie skierowanym
+# Ścieżka Hamiltona to ścieżka przechodząca przez wszystkie wierzchołki w grafie, przez każdy dokładnie raz. 
+# W ogólnym grafie znalezienie ścieżki Hamiltona jest problemem NP-trudnym. Proszę podać algorytm, który stwierdzi
+# czy istnieje ścieżka Hamiltona w acyklicznym grafie skierowanym (DAG).
 
-#przechodze po grafie dfs-gdy dojde do ostateniego wierzcholka dodaje go do res(tablica wynikowa)
-#gdy dfs sie cofa z jakiegos wierzcholka to dodaje go do res
-#na biezaco sprawdzam czy pomiedzy 2 ostatnio dodanymi wierzcholkami jest krawędź
-#gdyby jej nie było tzn ze nie posiada takiej sciezki
-#gdy nie wyrzuci nam bledu to zwracam res w odwroconej kolejnosci(bo dodawlam od konca)
-
-#sprawdza czy jest krawedz z wierzcholka u do v
+#sprawdza czy w grafie G jest krawedz z wierzcholka u do v
 def isEdge(G,u,v):
     for z in G[u]:
         if z == v:
@@ -29,49 +22,26 @@ def path(G):
         global flag
         visited[u] = True
 
-        for v in G[u]: #przeszukuje liste dzieci
+        for v in G[u]:
 
-            if not visited[v]: #jesli ten wiercholek nie byl odwiedzony
+            if not visited[v]: 
                 DFSvisit(v)
 
-        res.append(u) #po przetworzeniu wierzcholka dodaje go do wyniku
+        res.append(u) 
 
-        #działa
-        #bo dla grafu: 1
-        #             / \
-        #            2   3  strzałka od 1 do 2 i od 1 do 3 nie ma sciezki ham
-        #wiec najpierw by mi dodal 2 potem 3 i sprawdzi ze nie ma miedzy nimi krawedzi wiec zwroci False
-        if len(res) > 1: #sprawdzam czy od ostatniego do przedostaniego wierzcholka jest krawedz
-            #bo dodawane jest na odwrot do res
+        #sprawdzam czy od ostatniego do przedostaniego wierzcholka jest krawedz
+        if len(res) > 1:
             if not isEdge(G,res[len(res)-1],res[len(res) - 2]):
-                flag = False #gdyby nie bylo to nie moze posiadac tej sciezki
+                flag = False 
 
 
 
     for u in range(len(G)):
         if not visited[u]:
             DFSvisit(u)
-            if not flag: #gdy flaga jest False tzn ze brakowalo krawedzi wiec nie ma cyklu
+            #nie istnieje ścieżka hamiltona
+            if not flag: 
                 return False
-
-    return True,res[::-1] #bo dodawałam od tylu a jest skierowany
-
-#posiada
-G = [
-    [1],[3],[4],[2,4],[],
-]
-
-#brak
-G1 = [
-    [1],[3],[4],[4], [],
-]
-#brak
-G2 = [
-    [1,2],[],[],
-]
-
-#istnieje, zaczyna sie w wierzchołku 3
-G3 = [
-    [],[0],[1],[2],
-]
-print(path(G1))
+    
+    #zwraca ścieżkę hamiltona
+    return res[::-1] 
