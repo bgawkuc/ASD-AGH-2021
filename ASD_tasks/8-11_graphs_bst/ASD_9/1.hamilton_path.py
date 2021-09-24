94dc92db1b5d9c04419f1652362a92055567ecfd
Barbara Gaweł-Kucab <90979294+bgawkuc@users.noreply.github.com>
2021-09-24 17:22:58 +0200

    Update 1.hamilton_path.py

--- ASD_tasks/8-11_graphs_bst/ASD_9/1.hamilton_path.py
+++ ASD_tasks/8-11_graphs_bst/ASD_9/1.hamilton_path.py
@@ -1,4 +1,3 @@
-
 # Ścieżka Hamiltona to ścieżka przechodząca przez wszystkie wierzchołki w grafie, przez każdy dokładnie raz. 
 # W ogólnym grafie znalezienie ścieżki Hamiltona jest problemem NP-trudnym. Proszę podać algorytm, który stwierdzi
 # czy istnieje ścieżka Hamiltona w acyklicznym grafie skierowanym (DAG).
