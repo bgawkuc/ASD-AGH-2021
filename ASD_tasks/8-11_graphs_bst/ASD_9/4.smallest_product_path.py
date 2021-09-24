fa61f210bbcf837cbb9e3e199d71d03c1b2566aa
Barbara Gaweł-Kucab <90979294+bgawkuc@users.noreply.github.com>
2021-09-24 17:49:00 +0200

    Update 4.smallest_product_path.py

--- ASD_tasks/8-11_graphs_bst/ASD_9/4.smallest_product_path.py
+++ ASD_tasks/8-11_graphs_bst/ASD_9/4.smallest_product_path.py
@@ -1,20 +1,16 @@
-#ścieżka o najmniejszym iloczynie krawędzi z s do e
-#wagi krawędzi nalezą do liczb naturalnych dodatkich bez zera
+# Mamy dany graf G = (V, E) z wagami w∶ E → N−{0} (dodatnie liczby naturalne).
+# Chcemy znalezc scieżkę z wierzchołka u do v tak, by iloczyn wag był minimalny
 
-#wykonujemy zwykły algorytm dijikstry
-#z taką różnicą że bedzie brac wartosci logarytmow krawedzi
-#dzieki temu minimalizujac ich sume realnie zminimalizujemy iloczyn
-#bo: log(x) + log(y) = log(xy)
-#korzystam z log by nie miec DUŻYCH WARTOŚCI (iloczyn bardzo szybko rośnie)
-#wiec normalnie mozemy dodawac krawędzie(dodajemy logarytmy krawędzi)
-#ale bedzie tak na prawde nam sie tworzyła ściezka o min iloczynie
-#a nas interesuje odtworzenie ścieżki a parenci będą poprawni
-#O(ElogV)
+
+#Wykonujemy zwykły algorytm dijkstry, z różnicą, że będą brane wartości logarytmów krawedzi.
+#Dzięki temu minimalizujac sumę logarytmów, zminimalizujemy iloczyn krawędzi.
+#log(x) + log(y) = log(xy)
+
 
 from queue import PriorityQueue
 from math import log10
 
-def Dijikstra(G,s,e):
+def Dijkstra(G,s,t):
     n = len(G)
     inf = float("inf")
 
@@ -24,43 +20,24 @@ def Dijikstra(G,s,e):
     dist[s] = 0
 
     q = PriorityQueue()
-
-    for i in range(n):
-        if i != s:
-            q.put((inf,i))
-        else:
-            q.put((0,i))
-
+    q.put((0,s))
+    
     while not q.empty():
-        weight_u, u = q.get()
-
-        if not visited[u]:
-            visited[u] = True
-
-            for i in range(len(G[u])):
-                weight_v, v = G[u][i]
-                if dist[u] < inf and dist[v] > dist[u] + log10(weight_v):
-                    dist[v] = dist[u] + log10(weight_v)
-                    parent[v] = u
-                    q.put((dist[v],v))
+        _, u = q.get()
+        visited[u] = True
+        for edge,v in G[u]:
+            
+            #relaksacja, rozważam logarytm z wagi krawędzi
+            if not visited[v] and dist[v] > dist[u] + log10(edge):
+                dist[v] = dist[u] + log10(edge)
+                parent[v] = u
+                q.put((dist[v],v))
 
     path = []
-    last = e
-    while last != s:
+    last = t
+    
+    while last != None:
         path.append(last)
         last = parent[last]
-    path.append(s)
 
     return path[::-1]
-
-
-#waga,wierzcholek
-G = [
-    [(5,3),(3,4),(2,1)],
-    [(4,2)],
-    [(8,3),(1,4),(4,1)],
-    [(8,2),(5,0),(2,4)],
-    [(1,2),(3,0),(2,3)],
-]
-print(Dijikstra(G,0,2))
-#najkrotsza 0,4,2
