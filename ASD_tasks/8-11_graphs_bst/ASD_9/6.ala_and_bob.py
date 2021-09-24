63e076c6023855062d98b118dc07b98d23caaa47
Barbara Gaweł-Kucab <90979294+bgawkuc@users.noreply.github.com>
2021-09-24 18:19:35 +0200

    Update 6.ala_and_bob.py

--- ASD_tasks/8-11_graphs_bst/ASD_9/6.ala_and_bob.py
+++ ASD_tasks/8-11_graphs_bst/ASD_9/6.ala_and_bob.py
@@ -1,109 +1,81 @@
-#mam graf ktory reprezentuje miasta i odleglosc od nich
-#mam 2 kierowcow - Alicje i Boba
-#ustal kto ma 1 prowadzic tak by A przejechała jak najmniej km
-#wyznacz dlg tej trasy
+# Dana jest mapa kraju w postaci grafu G = (V, E), gdzie wierzchołki to miasta a krawędzie to drogi łączące miasta.
+# Dla każdej drogi znana jest jej długość (wyrażona w kilometrach jako liczba naturalna). Alicja i Bob prowadzą (na zmianę) 
+# autobus z miasta x ∈ V do miasta y ∈ V , zamieniając się za kierownicą w każdym kolejnym mieście. Alicja wybiera trasę 
+# oraz decyduje, kto prowadzi pierwszy. Proszę zapropnować algorytm, który wskazuje taką trasę (oraz osobę, która ma prowadzić pierwsza), żeby
+# Alicja przejechała jak najmniej kilometrów.
 
-#czyli mam 2 przypadki
-#1)1 krawędź jedzie A
-#2)1 krawędź jedzie B
-
-
-#liczę to dijkstrą z tą różnicą ze tablice parent, visited, i dist tworze 2-krotnie wieksze
-#jak mam idx od 0 do n - 1 to idx wierzchołka gdy prowadzi A do niego
-#jak mam idx od n do 2n - 1 to idx wierzchołka gdy prowadzi B do niego
-#np jak mam 4 wierzchołki to wierzchołkowi 1 odpowiada: 1(A do niego dojezdza) 5(1+4->B do niego dojezdza)
-#do kolejki wrzucam wierzcholki tylko 0 <-> n - 1 ale tez informacje kto do tego wierzchołka prowadził
-#i jak do u jechała A to sprawdzam czy dist dojazdu do v przez Boba da sie poprawic krawędzią u-v(jedzie nią B)
-#a jak do u jechał B to sprawdzam czy dist dojazdu do v przez Ale da sie poprawic krawedzia u-v(jedzie nia A)
-#i na koncu szukam minimum po dist[t] i dist[t+n] i wiem kto wjechal do wierzchołka t i i na bazie tego odtwarzam sciezke
+#Dijkstra, w której zostają podwojone tablice dist,visited,parent.
+#Indeksy 0-n-1 oznaczają, zę do i-tego wierzchołka jechała Ala.
+#Indeksy n-2n-1 oznaczają, zę do i-tego wierzchołka jechał Bob.
+#Do kolejki wrzucam informacje kto prowadził do danego wierzchołka.
+#Jeśli przez daną krawędź prowadzi Bob to liczę jej wartosć jako 0- bo minimalizuje trasę Ali.
 
 from queue import PriorityQueue
 
-#gdy prowadzi Ala to mam idx o wartosci 0 <-> n - 1
-#gdy prowadzi Bob to mam idx o wartosci n <-> 2n - 1
-
-def dijikstra(G,s,t):
+def dijkstra(G,s,t):
     n = len(G)
     inf = float("inf")
     q = PriorityQueue()
-
+    
+    #gdy prowadzi Ala to mam idx o wartosci 0 <-> n - 1
+    #gdy prowadzi Bob to mam idx o wartosci n <-> 2n - 1
     visited = [False] * (2*n)
     parent = [None] * (2*n)
     dist = [inf] * (2*n)
 
     dist[s] = 0
     dist[n+s] = 0
-
+    
+    #odległość od s, wierzchołek, kto kierował do ostatniego wierzchołka
     q.put((0,s,'A'))
     q.put((0,s,'B'))
 
-
     while not q.empty():
         _, u, driver = q.get()
+        
+        #gdy do u wjezdza A
+        if driver == 'A': 
+            visited[u+0] = True
+             for edge, v in G[u]:
 
-        if driver == 'A': #gdy do u wjezdza A
-            if not visited[u+0]: #to sprawdzam czy wierzcholek wjazdu Ali do u nie był jeszcze przetwarzany
-                visited[u+0] = True
+             #do v będzie jechał B, więc krawędź liczę jako 0, bo minimalizuje jedynie trasę A
+             if dist[v+n] > dist[u+0] + 0 and not visited[v+n]: 
+                dist[v+n] = dist[u+0] + 0
+                parent[v+n] = u + 0
+                q.put((dist[v+n],v,'B'))
+                
+        #gdy do u wjezdza B
+        elif driver == 'B': 
+            visited[u+n] = True
+            for edge,v in G[u]:
+                
+                #do v będzie jechała A, więc liczę normalnie krawędź
+                if dist[v+0] > dist[u+n] + edge and not visited[v+0]: 
+                    dist[v+0] = dist[u+n] + edge
+                    parent[v+0] = u + n
+                    q.put((dist[v+0],v,'A'))
 
-                for i in range(len(G[u])): #sprawdzam dzieci wychodzące z u
-                    edge, v = G[u][i]
-
-                    #sprawdzam czy jadąc przez krawedz z u do v da się poprawic dystans do v Boba
-                    #skoro do u prowadziła ala to do v bedzie prowadził Bob
-                    if dist[v+n] > dist[u+0] + 0 and not visited[v+n]: #krawędź gdzie prowadzi Bob licze jako 0
-                        dist[v+n] = dist[u+0] + 0
-                        parent[v+n] = u + 0
-                        q.put((dist[v+n],v,'B'))
-
-        elif driver == 'B': #gdy do u wjezdza B
-            if not visited[u+n]: #to sprawdzam wierzchołek wjazdu przez Boba czy nie był przetwarzany
-                visited[u+n] = True
-
-                for i in range(len(G[u])):
-                    edge, v = G[u][i]
-
-                    #teraz probuje poprawic wjazd do v przez Ale, gdy krawedz u-v przejezdza Ala
-                    #WAZNE TU JEST VISITED BY NIE BYŁO KWADRATU
-                    if dist[v+0] > dist[u+n] + edge and not visited[v+0]: #licze krawedz Ali
-                        dist[v+0] = dist[u+n] + edge
-                        parent[v+0] = u + n
-                        q.put((dist[v+0],v,'A'))
-
-
-    if dist[t+0] > dist[t+n]: #gdy taniej jest gdy to t wjezdza Bob
+    
+    #szukam kierowcy,który wjeżdża do wierzchołka t przy mniejszym dystansie jaki pokonała Ala
+    if dist[t+0] > dist[t+n]: 
         betterDist = dist[t+n]
-        curr = t + n #idx ostatniego wierzchołka
+        curr = t + n
     else:
-        betterDist = dist[t+0] #gdy taniej jest gdy to t wjezdza Ala
+        betterDist = dist[t+0] 
         curr = t + 0
-
+    
+    #odtwarzam ścieżkę
     path = []
-    first = inf
+    last =curr
 
-    while parent[curr] is not None:
-        if curr >= n:
-            path.append(curr-n)
-        else:
-            path.append(curr)
-        first, curr = curr, parent[curr]
-    path.append(s)
+    while curr is not None:
+        path.append(curr%n)
+        curr = parent[curr]
+        if curr is not None:
+            last = curr
 
-    if first >= n:
-        firstDriver = "Bob"
-    else:
-        firstDriver = "Ala"
-
-    return betterDist,path[::-1],firstDriver #ile km przejezdza Ala, trasa, kto zaczyna
-
-
-
-#(waga,wierzcholek)
-G = [
-    [(10,1),(1,2)],
-    [(10,0),(4,3),(7,4)],
-    [(1,0),(8,4)],
-    [(4,1),(6,5)],
-    [(8,2),(7,1),(5,5)],
-    [(6,3),(5,4)],
-]
-print(dijikstra(G,0,5))
+    if last >= n: firstDriver = "Ala"
+    else: firstDriver = "Bob"
+    
+    #zwraca: ilość km przejechanych przez Ale, trasę, kto prowadził jako pierwszy
+    return betterDist, path[::-1], firstDriver 
