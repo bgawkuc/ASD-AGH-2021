4fdb3b47502a48aa4b7d05e303e288ae8b5faff1
Barbara Gaweł-Kucab <90979294+bgawkuc@users.noreply.github.com>
2021-09-25 21:41:21 +0200

    Update BST_ceil_floor.py

--- Binary Search Tree/BST_ceil_floor.py
+++ Binary Search Tree/BST_ceil_floor.py
@@ -1,17 +1,6 @@
-#mamy drzewo BST
-#dla podanego klucza mam znaleźć jego sufit i podłogę
-#gdy klucz znajduje sie w drzewie to sufit i podłoga mają wartość tego klucza
-#a gdy nie to trzeba ich poszukac
-#wtedy podłoga to najwiekszy z mniejszych kluczy
-#a sufit to najmniejszy z wartosci wiekszych
-
-#modyfikuje operacje find
-#gdy znajde klucz o wartosci key to go zwracam jako sufit i podłoga
-#jesli key > root.key to wartosc PODŁOGI ustawiam na root.key
-#bo teraz bede szukac w wiekszych wartosciach, wiec jako podloge mogę ustawic tę wartosc bo jest mniejsza od klucza
-#jesli key < root.key to wartość SUFITU ustawiam na root.key
-#a skoro teraz bede szukac w mniejszych wartoscach to tę wartosc moge ustawic na sufiy bo jest wieksza od key
-
+#Dla zadanej wartości key znajduje suift i podłogę.
+#Sufit jest możliwie jak najmniejszą wartością, która jest większa lub równa od key.
+#Podłoga jest możliwie jak największą wartością, która jest mniejsza lub równa od key.
 
 
 class BST_Node:
@@ -21,46 +10,19 @@ class BST_Node:
         self.right = None
         self.parent = None
 
-
-def insert(root,key):
-    prev = None
-    while root is not None:
-        if key < root.key:
-            prev = root
-            root = root.left
-        else:
-            prev = root
-            root = root.right
-
-    if key < prev.key:
-        prev.left = BST_Node(key)
-        prev.left.parent = prev
-    else:
-        prev.right = BST_Node(key)
-        prev.right.parent = prev
-
-
 def find(root,key):
     ceil, floor = None, None
 
     while root is not None:
         if key == root.key:
             return "floor", key, "ceil", key
+        
         elif key < root.key:
             ceil = root.key
             root = root.left
+        
         else:
             floor = root.key
             root = root.right
+    
     return "floor:",floor,"ceil:", ceil
-
-
-root = BST_Node(6)
-insert(root,4)
-insert(root,8)
-insert(root,2)
-insert(root,1)
-insert(root,5)
-insert(root,7)
-insert(root,9)
-print(find(root,3))
