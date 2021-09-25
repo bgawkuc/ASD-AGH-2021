40fa1e9e4086361cc4510bdd476fc6569e7c663b
Barbara Gaweł-Kucab <90979294+bgawkuc@users.noreply.github.com>
2021-09-25 21:52:52 +0200

    Update binary_tree_to_BST.py

--- Binary Search Tree/binary_tree_to_BST.py
+++ Binary Search Tree/binary_tree_to_BST.py
@@ -1,8 +1,7 @@
-#mam drzewo binarne
-#mam je przekształcic w BST
+#Drzewo binarne przekształca w BST.
 
-#wyciagam klucze, sortuje je rosnaco
-#nastepnie rekurencyjnie tworze drzewo
+#Zapisuje wszystkie klucze z drzewa binarnego w tablicy i sortuje je rosnąco.
+#Na bazie tej tablicy tworzę BST.
 
 class Node:
     def __init__(self,key):
@@ -10,44 +9,6 @@ class Node:
         self.left = None
         self.right = None
 
-#wujmyje klucze
-def extractKeys(root,A):
-    if root is None:
-        return
-    extractKeys(root.left,A)
-    A.append(root.key)
-    extractKeys(root.right,A)
-
-#na bazie tablicy z kluczami tworze drzewo
-def convertToBST(root,A):
-    if root is None:
-        return
-    convertToBST(root.left,A)
-    root.key = next(A)
-    convertToBST(root.right,A)
-
-root = Node(8)
-root.left = Node(3)
-root.right = Node(2)
-root.right.left = Node(9)
-root.right.right = Node(10)
-
-
-A = []
-extractKeys(root,A)
-A.sort()
-A = iter(A)
-convertToBST(root,A)
-
-#sprawdzam czu dbrze zadziałało
-print(root.key)
-print(root.left.key)
-print(root.right.key)
-print(root.right.right.key)
-print(root.right.left.key)
-
-
-#2 SPOSÓB NA IMPLEMENTACJE
 def inorder(root):
     A = []
     def func(root):
@@ -64,25 +25,14 @@ def convert2BST(root):
     A.sort()
     idx = 0
 
-    #na bazie tablicy A tworzE drzwo BST taką samą procedurą co inorder
     def func(root):
         nonlocal idx
         if root is None:
             return
+        
         func(root.left)
         root.key = A[idx]
         idx += 1
         func(root.right)
 
     return func(root)
-
-
-root1 = Node(2)
-root1.left = Node(5)
-root1.right = Node(1)
-root1.left.left = Node(8)
-root1.left.right = Node(7)
-root1.right.left = Node(6)
-convert2BST(root1)
-
-
