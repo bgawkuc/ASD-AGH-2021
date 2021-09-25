0d921a45d8de6e490d5642d1c6dfcce730ee588f
Barbara Gaweł-Kucab <90979294+bgawkuc@users.noreply.github.com>
2021-09-25 21:36:39 +0200

    Update BST_average.py

--- Binary Search Tree/BST_average.py
+++ Binary Search Tree/BST_average.py
@@ -1,8 +1,7 @@
-#znajdz srednią wartosc w BST
-#czyli nalezy zsumowac klucze i podzielic sume przez ich ilość
+#Znajdź wartość średnią (średnia arytmetyczna) wśród wszystkich kluczy w drzewie.
 
-#licze sume i ilosc wezlów za pomocą rekurenycjnej funkcji inorder(tylko nie tworze tablicy z kluczami)
-#a po prostu sumuje klucze i zliczma ich ilosc
+#Przechodzę po drzewie w kolejności inorder.
+#Sumuje wszystkie klucze i liczę ilość.
 
 class BST_Node:
     def __init__(self,key):
@@ -11,30 +10,11 @@ class BST_Node:
         self.left = None
         self.right = None
 
-def insert(root,key):
-    prev = None
-    while root is not None: #szukam liścia do którego mam wstawic element
-        if key > root.key:
-            prev = root
-            root = root.right
-        else:
-            prev = root
-            root = root.left
-
-    if key < prev.key: #gdy klucz jest mniejszy od mojego liscia o staje sie jego lewym dzieckiem
-        prev.left = BST_Node(key)
-        prev.left.parent = prev #prev staje sie rodzicem mojego nowego Noda
-    else:
-        prev.right = BST_Node(key)
-        prev.right.parent = prev
-
 
 def average(root):
     sum_ = 0 #suma kluczy
     cnt = 0 #ilosc węzłów
 
-    #inorder to funkcja do wyswietlania w kolejknosci: lewy, root, prawy
-    #modyfikuje ją tak by liczyc sume kluczy i ilosc wezlów
     def inorder(root):
         nonlocal sum_,cnt
         if root is not None:
@@ -45,13 +25,3 @@ def average(root):
 
     inorder(root)
     return sum_/cnt
-
-root = BST_Node(6)
-insert(root,4)
-insert(root,8)
-insert(root,2)
-insert(root,1)
-insert(root,5)
-insert(root,7)
-insert(root,9)
-print(average(root))
