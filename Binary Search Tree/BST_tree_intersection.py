0d97f2963e517f415f6f0875a90905d4475c4d20
Barbara Gaweł-Kucab <90979294+bgawkuc@users.noreply.github.com>
2021-09-25 21:49:17 +0200

    Update BST_tree_intersection.py

--- Binary Search Tree/BST_tree_intersection.py
+++ Binary Search Tree/BST_tree_intersection.py
@@ -1,9 +1,8 @@
-#dostaje na wejściu dwa zbiory reprezentowane jako drzewa BST
-#znajdz ile jest w nich elementów wspólnyvh
-#przechodze oba drzewa inorder zpaisuje to w tablicach
-#inorder daje mi kolejność rosnącą
-#wiec teraz wysatrczy przechodzic po dwoch tablicach(nie dwie pętle a jedna!)
-#patrzac czy gdzies znajde taką samą wartość
+#Na wejściu znajdują się dwa drzewa BST.
+#Znajduje ilość kluczy, które występują w obu drzewach.
+
+#Zapisuje klucze 1 i 2 drzewa w kolejności inorder.
+#Przechodzę po wartościach kluczy i szukam elementów wspólnych.
 
 class BST_Node:
     def __init__(self,key,value):
@@ -14,25 +13,7 @@ class BST_Node:
         self.parent = None
 
 
-def insert(root,key,value):
-    prev = None
-    while root is not None:
-        if key > root.key:
-            prev = root
-            root = root.right
-        else:
-            prev = root
-            root = root.left
-
-    if key < prev.key:
-        prev.left = BST_Node(key,value)
-        prev.left.parent = prev
-    else:
-        prev.right = BST_Node(key,value)
-        prev.right.parent = prev
-
-
-#zapisuje klucze z drzewa rosnąco
+#zapisuje klucze z drzewa w tablicy A
 def inorderToArray(root,A):
     if root is not None:
         inorderToArray(root.left,A)
@@ -41,10 +22,11 @@ def inorderToArray(root,A):
     return A
 
 
+#znajduje ilość takich samych kluczy
 def intersection(root1,root2):
     A1 = inorderToArray(root1,[])
     A2 = inorderToArray(root2,[])
-    print(A1,A2)
+    
     i = j = 0
     cnt = 0
 
@@ -53,24 +35,11 @@ def intersection(root1,root2):
             cnt += 1
             i += 1
             j += 1
+        
         elif A1[i] < A2[j]:
             i += 1
+        
         else:
             j += 1
+    
     return cnt
-
-r1 = BST_Node(5,1)
-insert(r1,3,1)
-insert(r1,8,1)
-insert(r1,4,1)
-insert(r1,9,1)
-insert(r1,6,1)
-insert(r1,2,1)
-
-r2 = BST_Node(7,1)
-insert(r2,2,1)
-insert(r2,8,1)
-insert(r2,4,1)
-insert(r2,10,1)
-
-print(intersection(r1,r2))
